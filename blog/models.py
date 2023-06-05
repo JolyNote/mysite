c8d5497489ff43cb6a1cb73158eed8c11e63d295
JolyNote <quceqqwe@gmail.com>
2023-06-06 01:05:10 +0500

    Version 1.0

---
+++ blog/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64, verbose_name='название')
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
